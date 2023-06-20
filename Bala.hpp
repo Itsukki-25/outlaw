0bb496b5e39e4a017c82a2c1c1f0f306b005039f
Davi <77292587+See-Ivad@users.noreply.github.com>
2023-06-20 14:38:45 -0300

    Update Bala.hpp

--- Bala.hpp
+++ Bala.hpp
@@ -6,7 +6,7 @@ public:
     int coli = 0;
     int vida = 0;
 
-    int movimentos(Player jogador, sf::RectangleShape) {
+    int movimentos(Player jogador, sf::RectangleShape (*mapa)[40]) {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
             while (coli == 0) {
                 movimento(pos);
@@ -20,30 +20,30 @@ public:
             for (int i = 0; i < alt; i++) {
 			     for (int j = 0; j < larg; j++) {
 				     if(mapa[i][j].getFillColor() == sf::Color::Blue){
-					     if(jogador.getCorpo().getGlobalBounds().intersects(mapa[i][j].getGlobalBounds())){
-						     colidiu = true;
+					     if(corpo.getGlobalBounds().intersects(mapa[i][j].getGlobalBounds())){
+						     vida = 1;
+						     break;
 					     }
+
+						if (corpo.getGlobalBounds().intersects(mapa.getCasa)) {
+                    					coli = 1;
+
+                    				while (coli == 1) {
+                        				if (corpo.getGlobalBounds().intersects(jogador.getCorpo())) {
+                            					vida = 1;
+                            					break;
+                        				}
+
+                        				corpo.setPosition(pos[0], pos[1] * -1);
+
+                        				if (coli == 2) {
+                            					coli = 3;
+                        				}
+                    				}
+                			}
 				     }
 			     }
 		     }
-
-
-                if (corpo.getGlobalBounds().intersects(mapa.getCasa)) {
-                    coli = 1;
-
-                    while (coli == 1) {
-                        if (corpo.getGlobalBounds().intersects(jogador.getCorpo())) {
-                            vida = 1;
-                            break;
-                        }
-
-                        corpo.setPosition(pos[0], pos[1] * -1);
-
-                        if (coli == 2) {
-                            coli = 3;
-                        }
-                    }
-                }
             }
         }
 
