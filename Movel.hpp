5dbcb0f960ee9ca5638f6fb9f4c7309f0e68c0d4
Davi <77292587+See-Ivad@users.noreply.github.com>
2023-06-26 20:30:06 -0300

    :bug: Movel.hpp

--- Movel.hpp
+++ Movel.hpp
@@ -7,13 +7,13 @@ protected:
 	float pos[1],dir[1],vel;
 	bool colisao;
 	RectangleShape corpo;
-private:
+
 		bool tcolisao(bool coli,RectangleShape corpo){
 			this->colisao=coli;
 			this->corpo=corpo;
 			return coli;
 		}
-		virtual float Direção(float dir[1]){
+		virtual float Direcao(float dir[1]){
 			this-> dir[1]=dir[1];
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
@@ -28,8 +28,8 @@ private:
 			return dir[1];
 		}
 
-		virtual float movimento(float pos[0]){
-				this-> pos[0]=pos[0];
+		virtual float movimento(){
+				//this-> pos[0]=pos[0];
 
 				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
 					pos[1]=pos[1] +(dir[1]* vel);
