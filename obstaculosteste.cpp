a6a764be1cf61f3490b787c844248e3ad299a02c
Mageste <115092439+Itsukki-25@users.noreply.github.com>
2023-06-28 20:04:32 -0300

    Update obstaculosteste.cpp
    refiz tudo, funciona, porém, a bolinha não se move

--- obstaculosteste.cpp
+++ obstaculosteste.cpp
@@ -1,33 +1,60 @@
-//obstáculos(teste).cpp
+#include <SFML/Graphics.hpp>
+#include <iostream>
 /*
- * teste.cpp
- *
- *  Created on: 26 de mai. de 2023
- *      Author: Isabela
+ * funções contrutoras inicializam valores em uma classe <- descobri agr, 28/06, 17:34
  */
+class Movel {
+protected:
+	float tamanho = 10;
+	int posx;
+	int posy;
+	int velx = -5;
+	int vely = 5;
+	sf::Color cor = sf::Color::Green;
+	sf::CircleShape shape;
 
-/*
- * main.cpp
- *
- *  Created on: 26 de mai. de 2023
- *      Author: Isabela
- */
-//#include <SFML/Graphics.hpp>
-//#include <iostream>
-#include"obstaculos.hpp"
+public:
+	InicializaValores(sf::RenderWindow& window) {
+		posx = window.getSize().x / 2;
+		posy = window.getSize().y / 2;
+		shape.setRadius(tamanho);
+		shape.setFillColor(cor);
+		shape.setOrigin(tamanho / 2, tamanho / 2);
+		shape.setPosition(posx, posy);
+	}
+	void TestaLimite(sf::RenderWindow& window) {
+		vely = posy < window.getSize().y ? vely : -vely;
+		velx = posx < window.getSize().x ? velx : -velx;
+		//inverte se necessário
+		vely = posy > 0 ? vely : -vely;
+		velx = posx > 0 ? velx : -velx;
+	}
+	void Movimenta() {
+		posx = posx + velx * 1;
+		posy = posy + vely * 1;
+		shape.setPosition(posx, posy);
+		sf::sleep(sf::seconds(0.05));
+	}
+	void Desenha(sf::RenderWindow& window){
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+};
+
 int main() {
-	sf::RenderWindow window(sf::VideoMode(700, 540), "Bolinha ");
-	//int tempo = 1;
+	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML POOL!");
+//	int tempo = 1;
 
 //	float tamanho = 10;
-//
-//	int posx = 320;
-//	int posy = window.getSize().y;
+//	int posx = window.getSize().x / 2;
+//	int posy = window.getSize().y / 2;
 //	int velx = -5, vely = 5;
 //	sf::Color cor = sf::Color::Green;
 //	sf::CircleShape shape(tamanho);
 //	shape.setFillColor(cor);
-//	shape.setOrigin(tamanho / 2, tamanho);
+//	shape.setOrigin(tamanho / 2, tamanho / 2);
 //	shape.setPosition(posx, posy);
 
 	while (window.isOpen()) {
@@ -36,33 +63,32 @@ int main() {
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		std::string nome_txt = "boladefeno.jpeg";
-		Objeto_Movel bolinha;
-
-		//bolinha.Set_textura(nome_txt) ;
+		Movel bolinha;
+//		bolinha.Desenha();
+		bolinha.InicializaValores(window);
+		bolinha.TestaLimite( window);
+		bolinha.Movimenta();
+		bolinha.Desenha( window);
 
 //		{ //Teste Limites e inverte a velocidade se preciso
 //			vely = posy < window.getSize().y ? vely : -vely;
-//			//velx = posx < window.getSize().x? velx: -velx;
-//
+//			velx = posx < window.getSize().x ? velx : -velx;
+
 //			vely = posy > 0 ? vely : -vely;
-//			// velx = posx > 0? velx: -velx;
+//			velx = posx > 0 ? velx : -velx;
 //		}
 
-	//	std::cout << "px=" << posx << ";vx=" << velx;
-	//	std::cout << ";py=" << posy << ";vy=" << vely << std::endl;
+		//std::cout <<"px="<< posx <<";vx="<<velx;
+		//std::cout <<";py="<< posy <<";vy="<<vely<< std::endl;
 
-		//posx = window.getSize().x / 1.55;
-		//posx = 320;
-		//posx = tamanho / 2;
-		//posy = posy + vely * tempo;
-		//shape.setPosition(posx, posy);
+//		posx = posx + velx * tempo;
+//		posy = posy + vely * tempo;
+//		shape.setPosition(posx, posy);
+//		sf::sleep(sf::seconds(0.05));
 
-		window.clear();
-	//	window.draw( );
-		window.draw(bolinha.shape);
-		window.display();
-		//sf::sleep(sf::seconds(0.02));
+//		window.clear();
+//		window.draw(shape);
+//		window.display();
 	}
 
 	return 0;
