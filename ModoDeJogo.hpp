5cabef5d02cb94166e283840aa3b76db11973bd5
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2023-06-29 23:21:15 -0300

    Update ModoDeJogo.hpp

--- ModoDeJogo.hpp
+++ ModoDeJogo.hpp
@@ -1,14 +1,7 @@
-/*
- * ModoDeJogo.hpp
- *
- *  Created on: 19 de jun. de 2023
- *      Author: guima
- */
+
 
 #ifndef MODODEJOGO_HPP_
 #define MODODEJOGO_HPP_
-
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <stdlib.h>
@@ -17,7 +10,55 @@
 #include <iostream>
 #include "Bot.hpp"
 
+int modo(){
+	Botao Solo;
+	Botao dois;
+	Janela janela;
+	efeitos efeito;
 
+	Solo.setTexto(Text("1 VS IA", efeito.fonte, 24), efeito.fonte);
+	Solo.estrutura.setPosition(40, 50);
+
+	dois.setTexto(Text("1 VS 1",  efeito.fonte, 24),  efeito.fonte);
+	dois.estrutura.setPosition(Solo.posX -20, Solo.posY);
+
+	janela.executar();
+	{
+		while (janela.window.isOpen()) {
+			sf::Event event;
+			while (janela.window.pollEvent(event)) {
+
+				if (event.type == sf::Event::MouseButtonPressed)
+				{
+					if (event.mouseButton.button == sf::Mouse::Left)
+					{
+						sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
+						if (Solo.contemPonto(mousePos))
+						{
+							janela.window.clear();
+
+						}
+						if (dois.contemPonto(mousePos))
+						{
+							// Ação ao clicar no botão
+							janela.window.clear();
+						}
+					}
+				}
+			}
+		}
+	}
+	janela.window.clear();
+	janela.window.draw(Solo);
+	janela.window.draw(dois);
+	janela.window.display();
+
+	return  0;
+}
+
+
+
+#endif /* MODODEJOGO_HPP_ */
 
 
 int menu(){
@@ -66,7 +107,4 @@ int menu(){
 
 	return  0;
 }
-
-
-
 #endif /* MODODEJOGO_HPP_ */
