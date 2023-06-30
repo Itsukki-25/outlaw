52b7741ebead2e89d98adc19ef2da81b4fd8ad24
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2023-06-29 23:20:36 -0300

    Update Configurações.hpp

--- Configurações.hpp
+++ Configurações.hpp
@@ -1,96 +1,127 @@
-/*
- * config.cpp
- *
- *  Created on: 17 de jun. de 2023
- *      Author: guima
- */
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <iostream>
+
 #include "Bot.hpp"
+
 using namespace sf;
 using namespace std;
 
-class controle : Botao{
+class Controle : public Botao {
 public:
-	sf::RectangleShape Base;
-	Botao salvar,sair,som,musica;
+	sf::RectangleShape Base,lin1,lin2;
+	Botao salvar, sair, som, musica,linha1,linha2;
+
 private:
 	Janela janela;
-	void config(){
+	efeitos fonte;
 
-		float x =janela.window.getSize() -50;
-		float y = janela.window.getSize()- 50;
-		Base.setScale(x,y);
-		salvar.setTexto(Text("salvar", fonte, 24), fonte);
+	void config()
+	{
+		fonte.fonte.loadFromFile("assets/font.ttf");
+
+		float x = janela.screenSize.x - 50;
+		float y = janela.screenSize.y - 50;
+		Base.setSize(Vector2f(x, y));
+
+		salvar.setTexto(Text("Salvar", fonte.fonte, 24), fonte.fonte);
 		salvar.estrutura.setPosition(x + 20, y + 5);
 
-		sair.setTexto(Text("Começar", fonte, 24), fonte);
-		sair.estrutura.setPosition( salvar.posX + 20, y + 5);
+		sair.setTexto(Text("Começar", fonte.fonte, 24), fonte.fonte);
+		sair.estrutura.setPosition(salvar.posX + 20, y + 5);
 
-		som.setTexto(Text("Som", fonte, 24), fonte);
+		som.setTexto(Text("Som", fonte.fonte, 24), fonte.fonte);
 		som.estrutura.setPosition(salvar.posX, y + 70);
 
-		musica.setTexto(Text("Musica", fonte, 24), fonte);
-		musica.estrutura.setPosition(salvar.posX, som.posY -20);
+		musica.setTexto(Text("Musica", fonte.fonte, 24), fonte.fonte);
+		musica.estrutura.setPosition(salvar.posX, som.posY - 20);
 
-				janela.window.draw(Base);
+		janela.window.draw(Base);
 		janela.window.draw(salvar);
 		janela.window.draw(sair);
 		janela.window.draw(som);
 		janela.window.draw(musica);
-	}
-
-	void padrao(){
-		//musica on -> 100%
-		//som on -> 100%
 
 	}
-	void teste(){
-		janela.executar();
+
+	void padraoMusica()
+	{
+		if (fonte.musica ==0){
+			fonte.musica==75;
+		}else{
+			fonte.musica=0;
+		}
+
+	}
+	void padraosom(){
+		if (fonte.clique ==0){
+			fonte.clique==75;
+		}else{
+			fonte.clique=0;
+		}
+	}
+
+
+	void teste()
+	{
+		while (janela.window.isOpen())
+		{
+			bool Som,Musica;
+			sf::Event event;
+			while (janela.window.pollEvent(event))
+			{
+				if (event.type == sf::Event::MouseButtonPressed)
 				{
-					while (janela.window.isOpen()) {
-						sf::Event event;
-						while (janela.window.pollEvent(event)) {
+					if (event.mouseButton.button == sf::Mouse::Left)
+					{
+						sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
 
-							if (event.type == sf::Event::MouseButtonPressed)
-							{
-								if (event.mouseButton.button == sf::Mouse::Left)
-								{
-									sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
-									if (som.contemPonto(mousePos))
-									{
-										// Ação ao clicar no botão
 
-									}
-									if (musica.contemPonto(mousePos))
-									{
-										// Ação ao clicar no botão
+						if (som.contemPonto(mousePos))
+						{
+							// Ação ao clicar no botão "Som"
+							padraosom();
+							som=true;
 
-									}
-									if (salvar.contemPonto(mousePos))
-									{
-										// Ação ao clicar no botão
+						}
+						if (musica.contemPonto(mousePos))
+						{
+							// Ação ao clicar no botão "Musica"
+							padraoMusica();
+							musica=true;
 
-									}
-									if (sair.contemPonto(mousePos))
-									{
-										// Ação ao clicar no botão
+						}
+						if (salvar.contemPonto(mousePos))
+						{
+							// Ação ao clicar no botão "Salvar"
+							janela.window.close();
 
-									}
-
-								}
+						}
+						if (sair.contemPonto(mousePos))
+						{
+							// Ação ao clicar no botão "Começar"
+							if (musica==true){
+								padraoMusica();
 							}
+							if (som==true){
+								padraosom();
+							}
+							janela.window.close();
 						}
 					}
 				}
-
+			}
+		}
 	}
 
-
-
+public:
+	void executar() override
+	{
+		config();
+		teste();
+	}
 };
+
