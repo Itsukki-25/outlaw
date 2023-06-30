d29446e79ffef93cdd84714958490ed38daf674f
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2023-06-29 23:19:32 -0300

    Update Bot.hpp

--- Bot.hpp
+++ Bot.hpp
@@ -1,66 +1,63 @@
-
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include <iostream>
 #include <chrono>
 #include <random>
 #include <thread>
 #include "Bot.hpp"
 #include "Movel.hpp"
-//#include "Bala.hpp"
+#include "Bala.hpp"
+#include "Player.hpp"
+
 using namespace sf;
 using namespace std;
-class bot {
-public:
-	sf::RectangleShape corpo;
 
+class Bot : public Player {
+public:
+    sf::RectangleShape corpo;
+    sf::ConvexShape olho;
 
 private:
-	void movimento(){
-		int intervaloTempo = 1;
+    void movimento() {
+        int intervaloTempo = 1;
 
+        bool vivo = true;
+        bool andar = true;
+        float X = corpo.getPosition().x;
+        float Y = corpo.getPosition().y;
+        olho.setPoint(0, sf::Vector2f(X, Y));
+        olho.setPoint(1, sf::Vector2f(X + 100, Y - 50));
+        olho.setPoint(2, sf::Vector2f(X + 100, Y));
+        olho.setPoint(2, sf::Vector2f(X + 100, Y - 50));
 
-		while (true) {
-			auto tempoInicio = std::chrono::steady_clock::now();
+        while (vivo) {
+            auto tempoInicio = std::chrono::steady_clock::now();
 
+            while (andar) {
+                auto tempoAtual = std::chrono::steady_clock::now();
+                auto duracao = std::chrono::duration_cast<std::chrono::seconds>(tempoAtual - tempoInicio).count();
 
-			while (true) {
+                std::random_device rd;
+                std::mt19937 gen(rd());
+                std::uniform_int_distribution<> dis(-1, 1);
+                int posX = dis(gen);
+                int posY = dis(gen);
 
-				auto tempoAtual = std::chrono::steady_clock::now();
-				auto duracao = std::chrono::duration_cast<std::chrono::seconds>(tempoAtual - tempoInicio).count();
-				std::srand(std::time(nullptr));
+                int velX = posX * 10;
+                int velY = posY * 10;
+                corpo.move(velX, velY);
 
-				std::random_device rd;
-				std::mt19937 gen(rd());
-				std::uniform_int_distribution<> dis(0, 2);
-				int posX = dis(gen) - 1;
+                if (duracao >= intervaloTempo) {
+                    break;
+                }
 
-				std::random_device rd;
-				std::mt19937 gen(rd());
-				std::uniform_int_distribution<> dis(0, 2);
-				int posY = dis(gen) - 1;
+                olho.move(posX, posY);
+                if (olho.getGlobalBounds().intersects(Player::getCorpo().getGlobalBounds())) {
+                    // Ação ao colidir com o jogador
+                }
+            }
 
-				int velX=posX * 10;
-				int velY=posY * 10;
-				corpo.setPosition(velX, velY);
-				if (duracao >= intervaloTempo) {
-					break;
-				}
-			}
-
-
-			std::cout << "Passaram-se 3 segundos!" << std::endl;
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-		}
-
-
-
-	}
-
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
 };
