3af99664b9cc4c69385722a8b62db8d90c2ae158
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2023-06-29 23:21:32 -0300

    Update Movel.hpp

--- Movel.hpp
+++ Movel.hpp
@@ -1,51 +1,56 @@
-using namespace std;
-using namespace sf;
+#ifndef MOVEL_HPP_
+#define MOVEL_HPP_
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
 
-class movel{
+class Movel {
+public:
+    float pos[2], dir[2], vel;
+    bool colisao;
+    sf::RectangleShape corpos;
+
 protected:
-	float pos[1],dir[1],vel;
-	bool colisao;
-	RectangleShape corpo;
+    bool tcolisao(bool coli, sf::RectangleShape corpo) {
+        this->colisao = coli;
+        this->corpos = corpo;
+        return coli;
+    }
 
-		bool tcolisao(bool coli,RectangleShape corpo){
-			this->colisao=coli;
-			this->corpo=corpo;
-			return coli;
-		}
-		virtual float Direcao(float dir[1]){
-			this-> dir[1]=dir[1];
+    virtual float Direcao(float dir[1] = 0) {
+        this->dir[1] = dir[1];
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            dir[1] = 1;
+        }
 
-				dir[1]=1;
-			}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            dir[1] = -1;
+        }
 
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+        return dir[1];
+    }
 
-				dir[1]=-1;
-			}
-			return dir[1];
-		}
+    virtual float movimento(float pos[0]) {
+        this->pos[0] = pos[0];
 
-		virtual float movimento(){
-				//this-> pos[0]=pos[0];
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            pos[1] = pos[1] + (dir[1] * vel);
+        }
 
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-					pos[1]=pos[1] +(dir[1]* vel);
-				}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            pos[1] = pos[1] + (dir[1] * vel);
+        }
 
-				if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-					pos[1]=pos[1] +(dir[1]* vel);
-				}
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-					pos[0]=pos[0] + (vel*1);
-				}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            pos[0] = pos[0] + (vel * 1);
+        }
 
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-					pos[0]=pos[0] + (vel*-1);
-							}
-				return pos[1] and pos[0];
-			}
+        return pos[0];
+    }
 
+    virtual ~Movel() {}
 };
+
+#endif /* MOVEL_HPP_ */
