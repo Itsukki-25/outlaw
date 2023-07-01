161fcc96bc55f625b58829fb61e5573ffe7073a8
Davi <77292587+See-Ivad@users.noreply.github.com>
2023-07-01 16:34:51 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
@@ -8,32 +7,42 @@
 #include "player.hpp"
 #include "Bala.hpp"
 
+
 using namespace std;
 
-vector<bala> balas;
+vector<Bala> balas;
 
-void desenharBala(sf::RenderWindow& window, Mapa mapa, Player jogador){
-	for(int i = 0; i <= balas.size(); i++){
-		balas[i].movimentos(jogador, mapa.getCasa(), 20, 40);
-		window.draw(balas[i].corpo);
+void desenharBala(sf::RenderWindow& window, Mapa mapa, Player& jogador){
+
+	if(balas.size())
+	for(int i = 0; i <= balas.size()-1 ; i++){
+
+		window.draw(balas[i].getCorpo());
+
+		if(!balas[i].getVida())
+			balas.erase(balas.begin() + i);
+
+		if(!balas[i].movimentos(jogador, mapa.getCasa(), 20, 40))
+			break;
 	}
+
 };
 
-int main() {
+int main(){
 
 	vector<string> mapa1{"########################################",
-			 	 	 	 "#      #                               #",
-						 "#      #               #               #",
-						 "#      #        *      #               #",
-						 "#      #        *      #               #",
-						 "#      #               #        #      #",
-						 "#      #  #            #         #     #",
-						 "#       ##             #          #    #",
+			 	 	 	 "#                                      #",
+						 "#                      #               #",
+						 "#    2          *      #               #",
+						 "#               *      #               #",
+						 "#                      #        #      #",
+						 "#                      #         #     #",
+						 "#                      #          #    #",
 						 "#                     *                #",
-						 "#                     *           *    #",
-						 "#     *               *           *    #",
+						 "#                     *          *     #",
+						 "#     *     ######    *          *     #",
 						 "#     *                                #",
-						 "#                                      #",
+						 "#                               1      #",
 						 "#                   ########           #",
 						 "#                           #          #",
 						 "#        #     #            #     #    #",
@@ -43,7 +52,6 @@ int main() {
 						 "########################################"};
 
 
-
 	const int WIDTH = 1280;
 	const int HEIGHT = 640;
 
@@ -51,18 +59,22 @@ int main() {
 	const int colunas = 40;
 	int SQUARE_SIZE = 32;
 
+	bool jogador1Tecl1 = false, jogador1Tecl2 = false;
+	bool jogador2Tecl1 = false, jogador2Tecl2 = false;
+
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Outlaw");
 	window.setFramerateLimit(60);
 
 	Mapa fase1(colunas, linhas, SQUARE_SIZE);
 
-	fase1.definirValorCasas(mapa1);
-
 	Player jogador1;
 	jogador1.iniciarPlayer();
+	jogador1.setCodigo(1);
 	Player jogador2;
-	jogador2.setPosi(400, 200);
 	jogador2.iniciarPlayer();
+	jogador2.setCodigo(2);
+
+	fase1.definirValorCasas(mapa1, jogador1, jogador2);
 
 	while (window.isOpen()){
 
@@ -73,44 +85,78 @@ int main() {
 	    		std::cout << "Programa finalizado manualmente!" << std::endl;
 	    		window.close();
 	        }
+
+		    if(sf::Keyboard::isKeyPressed(sf::Keyboard::RShift)){
+		    	jogador1Tecl1 = true;
+		    	jogador1Tecl2 = false;
+		    }else{
+		    	jogador1Tecl2= true;
+		    }
+		    if(event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Up){
+		    	jogador1Tecl2= true;
+		    }
+		    if(event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Down){
+		    	jogador1Tecl2= true;
+		    }
+		    if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)){
+		    	jogador2Tecl1 = true;
+		    	jogador2Tecl2 = false;
+		    }else{
+		    	jogador2Tecl2= true;
+		    }
+		    if(event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::W){
+		    	jogador2Tecl2= true;
+		    }
+		    if(event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::S){
+		    	jogador2Tecl2= true;
+		    }
+
 	    }
 
-	    if(sf::Keyboard::isKeyPressed(sf::Keyboard::RShift)){
-	    	jogador1.setAltura(-62,fase1.getCasa(), linhas, colunas);
-	    }else{
-	    	jogador1.setAltura(-120,fase1.getCasa(), linhas, colunas);
+	    if(jogador1Tecl1 == true && jogador1Tecl2 == true){
+	    	std::cout << "Jogador 1 atirou" <<std::endl;
+	    	balas.push_back(Bala(-1, jogador1.getVelY(), jogador1));
+	    	jogador1Tecl1 = false;
+	    	jogador1Tecl2 = false;
+
 	    }
-	    if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)){
-	    	jogador2.setAltura(-60,fase1.getCasa(), linhas, colunas);
-	    }else{
-	    	jogador2.setAltura(-120,fase1.getCasa(), linhas, colunas);
+	    if(jogador2Tecl1 == true && jogador2Tecl2 == true){
+	    	std::cout << "Jogador 2 atirou" <<std::endl;
+	    	balas.push_back(Bala(1, jogador2.getVelY(), jogador2));
+	    	jogador2Tecl1 = false;
+	    	jogador2Tecl2 = false;
+
 	    }
 
     	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-    		jogador1.setVelX(-2);
-    	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-    		jogador1.setVelX(2);
-    	}
-    	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
     		jogador1.setVelY(-2);
-    	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+    	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
     		jogador1.setVelY(2);
     	}
+    	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+    		jogador1.setVelX(-2);
+    	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+    		jogador1.setVelX(2);
+    	}
     	if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
-    		jogador2.setVelX(-2);
+    		jogador2.setVelY(-2);
     	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-    		jogador2.setVelX(2);
+    		jogador2.setVelY(2);
     	}
     	if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-    		jogador2.setVelY(-2);
+    		jogador2.setVelX(-2);
     	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
-    		jogador2.setVelY(2);
+    		jogador2.setVelX(2);
     	}
 
 	    window.clear();
 	    fase1.desenharMapa(window);
-	    jogador1.movePlayer(fase1.getCasa(), linhas, colunas);
-	    jogador2.movePlayer(fase1.getCasa(), linhas, colunas);
+	    desenharBala(window, fase1, jogador1);
+	    desenharBala(window, fase1, jogador2);
+	    if(!sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
+	    	jogador1.movePlayer(fase1.getCasa(), linhas, colunas);
+	    if(!sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+	    	jogador2.movePlayer(fase1.getCasa(), linhas, colunas);
 	    jogador1.desenharPlayer(window);
 	    jogador2.desenharPlayer(window);
 	    window.display();
