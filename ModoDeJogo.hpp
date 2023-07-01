2fc1ae13add2515201599cf599f1cc1e902272a4
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2023-07-01 13:32:24 -0300

    Update ModoDeJogo.hpp

--- ModoDeJogo.hpp
+++ ModoDeJogo.hpp
@@ -1,7 +1,13 @@
-
+/*
+ * ModoDeJogo.hpp
+ *
+ *  Created on: 19 de jun. de 2023
+ *      Author: guima
+ */
 
 #ifndef MODODEJOGO_HPP_
 #define MODODEJOGO_HPP_
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <stdlib.h>
@@ -9,102 +15,46 @@
 #include <time.h>
 #include <iostream>
 #include "Bot.hpp"
+#include "janela.hpp"
+#include "efeitos.hpp"
+#include "Textos.hpp"
 
-int modo(){
+class Modo {
+public:
 	Botao Solo;
 	Botao dois;
-	Janela janela;
+	Janela *janela;
 	efeitos efeito;
+	Textos Mododejogo;
+	int *controlPanel;
 
-	Solo.setTexto(Text("1 VS IA", efeito.fonte, 24), efeito.fonte);
-	Solo.estrutura.setPosition(40, 50);
+	void escolha() {
+		Mododejogo.adicionarTexto("Modo de jogo", 100, 200, 100);
+		Solo.setTexture(efeito.Tbotao);
+		Solo.setTexto(Text("1 VS IA", efeito.fonte, 52), efeito.fonte,750,300);
+		Solo.estrutura.setPosition(700, 300);
 
-	dois.setTexto(Text("1 VS 1",  efeito.fonte, 24),  efeito.fonte);
-	dois.estrutura.setPosition(Solo.posX -20, Solo.posY);
 
-	janela.executar();
-	{
-		while (janela.window.isOpen()) {
-			sf::Event event;
-			while (janela.window.pollEvent(event)) {
+		dois.setTexture(efeito.Tbotao);
+		dois.setTexto(Text("1 VS 1", efeito.fonte, 52), efeito.fonte,350,300);
+		dois.estrutura.setPosition(300,300);
 
-				if (event.type == sf::Event::MouseButtonPressed)
-				{
-					if (event.mouseButton.button == sf::Mouse::Left)
-					{
-						sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
-						if (Solo.contemPonto(mousePos))
-						{
-							janela.window.clear();
+		sf::Vector2i mousePos = sf::Mouse::getPosition(janela->window);
 
-						}
-						if (dois.contemPonto(mousePos))
-						{
-							// Ação ao clicar no botão
-							janela.window.clear();
-						}
-					}
-				}
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+			if (Solo.contemPonto(mousePos)) {
+				std::cout << "TELA DIFERENTE1\n";
+			}
+			if (dois.contemPonto(mousePos)) {
+				std::cout << "TELA DIFETENTE2\n";
 			}
 		}
+		janela->window.draw(efeito.Modo);
+		Mododejogo.desenharTextos(janela->window);
+		janela->window.draw(Solo);
+		janela->window.draw(dois);
 	}
-	janela.window.clear();
-	janela.window.draw(Solo);
-	janela.window.draw(dois);
-	janela.window.display();
 
-	return  0;
-}
+};
 
-
-
-#endif /* MODODEJOGO_HPP_ */
-
-
-int menu(){
-	Botao Solo;
-	Botao dois;
-	Janela janela;
-	fonte.loadFromFile("C:/Users/guima/eclipse-workspace/Assets.flavor-sans.otp");
-
-
-	Solo.setTexto(Text("1 VS IA", fonte, 24), fonte);
-	Solo.estrutura.setPosition(40, 50);
-
-	dois.setTexto(Text("1 VS 1", fonte, 24), fonte);
-	dois.estrutura.setPosition(Solo.posX -20, Solo.posY);
-
-	janela.executar();
-	{
-		while (janela.window.isOpen()) {
-			sf::Event event;
-			while (janela.window.pollEvent(event)) {
-
-				if (event.type == sf::Event::MouseButtonPressed)
-				{
-					if (event.mouseButton.button == sf::Mouse::Left)
-					{
-						sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
-						if (Solo.contemPonto(mousePos))
-						{
-							// Ação ao clicar no botão
-
-						}
-						if (dois.contemPonto(mousePos))
-						{
-							// Ação ao clicar no botão
-
-						}
-					}
-				}
-			}
-		}
-	}
-	janela.window.clear();
-	janela.window.draw(Solo);
-	janela.window.draw(dois);
-	janela.window.display();
-
-	return  0;
-}
 #endif /* MODODEJOGO_HPP_ */
