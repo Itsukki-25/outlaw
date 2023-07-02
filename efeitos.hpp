368401d192eeb8ba9be969a15e812c743056c793
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2023-07-02 18:31:30 -0300

    Update efeitos.hpp

--- efeitos.hpp
+++ efeitos.hpp
@@ -1,56 +1,75 @@
-/*
- * efeitos.hpp
- *
- *  Created on: 30 de jun. de 2023
- *      Author: guima
- */
-
 #ifndef EFEITOS_HPP_
 #define EFEITOS_HPP_
+
 class efeitos {
 public:
-    sf::Font fonte;
-    sf::SoundBuffer SomDoTiro;
-    sf::Sound tiros;
-    sf::Music musica;
-    sf::SoundBuffer SomDoClique;
-    sf::Sound clique;
-    sf::Texture textureFundo;
-    sf::Sprite fundoImage;
-    sf::Texture TexturaBotao;
-    sf::Texture FundoMododejogo;
-    sf::Sprite FundoModo;
+	sf::Font fonte;
+	sf::SoundBuffer SomDoTiro;
+	sf::Sound tiros;
+	sf::Music musica;
+	sf::SoundBuffer SomDoClique;
+	sf::Sound clique;
+	sf::Texture textureFundo;
+	sf::Sprite fundoImage;
+	sf::Texture TexturaBotao;
+	sf::Texture FundoMododejogo;
+	sf::Sprite FundoModo;
+	sf::Texture FundoDasConfig;
+	sf::Sprite FundoParaAsConfig;
+	int sons = 0;
+	int musicas = 0;
 
-    	efeitos()
-    {
-    	FundoMododejogo.loadFromFile("assets/Modo.jpg");
-    	FundoModo.setTexture(FundoMododejogo);
-    	TexturaBotao.loadFromFile("assets/Tbotao.jpg");
-    	textureFundo.loadFromFile("assets/cowboy.jpg");
+	efeitos()
+	{
+		FundoDasConfig.loadFromFile("assets/Config.png");
+		FundoParaAsConfig.setTexture(FundoDasConfig);
+		FundoMododejogo.loadFromFile("assets/Modo.jpg");
+		FundoModo.setTexture(FundoMododejogo);
+		TexturaBotao.loadFromFile("assets/Tbotao.jpg");
+		textureFundo.loadFromFile("assets/cowboy.jpg");
 		fundoImage.setTexture(textureFundo);
-        fonte.loadFromFile("assets/fonte.TTF");
-        SomDoClique.loadFromFile("assets/clique.ogg");
-        clique.setBuffer(SomDoClique);
-        musica.openFromFile("assets/musica.ogg");
+		fonte.loadFromFile("assets/fonte.TTF");
+		SomDoClique.loadFromFile("assets/clique.ogg");
+		clique.setBuffer(SomDoClique);
+		musica.openFromFile("assets/musica.ogg");
+	}
 
-    }
+	bool DesligarSom(bool tocar, int volume)
+	{
+		if (tocar) {
+			if (clique.getStatus() == sf::SoundSource::Playing) {
+				clique.stop();
+				return false;
+			} else {
+				clique.play();
+				return true;
+			}
+		} else {
+			clique.stop();
+			return false;
+		}
+	}
 
-    	void cliqueBotao(){
-    		clique.play();
-    	}
-
-    	void IniciarMusica(int tocar){
-    		if (tocar==1){
-    			musica.setLoop(true);
-    			    		musica.play();
-    		}
-    		else{
-    			musica.stop();
-    		}
-
-    	}
+	void Sons(bool tocar)
+	{
+	    if (tocar) {
+	        clique.play();
+	    }
+	}
 
 
+	void Musica(bool tocar, int volume)
+	{
+		if (tocar) {
+			if (musica.getStatus() == sf::SoundSource::Playing) {
+				musica.stop();
+			} else {
+				musica.play();
+			}
+		} else {
+			musica.stop();
+		}
+	}
 };
 
 #endif /* EFEITOS_HPP_ */
