66102d1abd0cee7e8491e9b23f32d0228416e562
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2023-07-01 23:56:03 -0300

    Update Botão.hpp

--- Botão.hpp
+++ Botão.hpp
@@ -4,6 +4,7 @@
  *  Created on: 8 de jun. de 2023
  *      Author: guima
  */
+
 #include <SFML/Graphics.hpp>
 #include <SFML/audio.hpp>
 #include <stdlib.h>
@@ -12,25 +13,34 @@
 #include <iostream>
 #include "efeitos.hpp"
 using namespace sf;
+
 #ifndef DECLARACOES_HPP_
 #define DECLARACOES_HPP_
+
+
+
 
 class Botao : public Drawable {
 public:
 
-    RectangleShape estrutura;
+    RectangleShape estrutura,sombras;
+
     Text texto;
     float posX,posY;
+
 
     Botao(float largura = 300, float comprimento = 80, Color cor = Color::White,float posx=0,float posY=0,bool clique=false)
 
         : estrutura(Vector2f(largura, comprimento)), texto()
+
     {
          this-> posX=posx;
         this-> posY=posY;
+
         estrutura.setFillColor(cor);
         estrutura.setPosition(posx,posY);
     }
+
 
     void setTexture(const sf::Texture& textura)
        {
@@ -44,13 +54,17 @@ public:
         texto.setPosition(posx,posy);
     }
     void draw(RenderTarget& target, RenderStates states) const override {
-        target.draw(estrutura, states);
+
+    	target.draw(estrutura, states);
+
         target.draw(texto, states);
     }
+
+
     bool contemPonto(const sf::Vector2i& ponto) {
 
         return estrutura.getGlobalBounds().contains(sf::Vector2f(ponto.x, ponto.y));
     }
-
 };
-#endif /* DECLARACOES_HPP_ *///Toma sua cu
+#endif /* DECLARACOES_HPP_ *///
+
