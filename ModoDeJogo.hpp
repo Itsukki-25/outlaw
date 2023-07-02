0d02c304d0e3dc0dcda3513e6fdec128068b274a
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2023-07-01 23:56:56 -0300

    Update ModoDeJogo.hpp

--- ModoDeJogo.hpp
+++ ModoDeJogo.hpp
@@ -18,39 +18,47 @@
 #include "janela.hpp"
 #include "efeitos.hpp"
 #include "Textos.hpp"
-
+#include "Sombras.hpp"
 class Modo {
 public:
 	Botao Solo;
 	Botao dois;
+	Sombra SombraUm;
+	Sombra SombraDois;
 	Janela *janela;
 	efeitos efeito;
 	Textos Mododejogo;
+
 	int *controlPanel;
 
 	void escolha() {
 		Mododejogo.adicionarTexto("Modo de jogo", 100, 200, 100);
-		Solo.setTexture(efeito.Tbotao);
+		Solo.setTexture(efeito.TexturaBotao);
 		Solo.setTexto(Text("1 VS IA", efeito.fonte, 52), efeito.fonte,750,300);
 		Solo.estrutura.setPosition(700, 300);
+		SombraUm.Comportamento(300,80,710,310);
 
 
-		dois.setTexture(efeito.Tbotao);
+		dois.setTexture(efeito.TexturaBotao);
 		dois.setTexto(Text("1 VS 1", efeito.fonte, 52), efeito.fonte,350,300);
 		dois.estrutura.setPosition(300,300);
-
+		SombraDois.Comportamento(300,80,310,310);
 		sf::Vector2i mousePos = sf::Mouse::getPosition(janela->window);
 
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 			if (Solo.contemPonto(mousePos)) {
+				efeito.cliqueBotao();
 				std::cout << "TELA DIFERENTE1\n";
 			}
 			if (dois.contemPonto(mousePos)) {
+				efeito.cliqueBotao();
 				std::cout << "TELA DIFETENTE2\n";
 			}
 		}
-		janela->window.draw(efeito.Modo);
+		janela->window.draw(efeito.FundoModo);
 		Mododejogo.desenharTextos(janela->window);
+		janela->window.draw(SombraUm);
+		janela->window.draw(SombraDois);
 		janela->window.draw(Solo);
 		janela->window.draw(dois);
 	}
