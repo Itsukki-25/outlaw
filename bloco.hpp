83d75447224d59eae24ab633e6051ac9ccde5117
Davi <77292587+See-Ivad@users.noreply.github.com>
2023-07-04 11:45:45 -0300

    Create bloco.hpp

---
+++ bloco.hpp
@@ -0,0 +1,26 @@
+/*
+ * bloco.hpp
+ *
+ *  Created on: 4 de jul. de 2023
+ *      Author: user
+ */
+
+#ifndef BLOCO_HPP_
+#define BLOCO_HPP_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+class Bloco{
+public:
+
+	sf::RectangleShape corpo;
+	bool solido;
+
+};
+
+
+
+#endif /* BLOCO_HPP_ */
