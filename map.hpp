0833c1635edad39c945c683a53b470c4c5a1a6bf
Davi <77292587+See-Ivad@users.noreply.github.com>
2023-07-04 11:47:19 -0300

    Update map.hpp

--- map.hpp
+++ map.hpp
@@ -10,6 +10,7 @@
 #include <SFML/Graphics.hpp>
 
 #include "player.hpp"
+#include "bloco.hpp"
 
 class Mapa{
 private:
@@ -17,7 +18,7 @@ private:
 	int largura;
 	int altura;
 	int tamanhoCasa;
-	sf::RectangleShape casas[40][40];
+	Bloco casas[40][40];
 
 public:
 
@@ -39,34 +40,48 @@ public:
 		std::cout << "Objeto 'Mapa' criado!" << std::endl;
 	}
 
-	void definirValorCasas(std::vector<std::string>& mapa, Player& jogador1, Player& jogador2 ) {
+	void definirValorCasas(std::vector<std::string>& mapa, Player& jogador1, Player& jogador2, sf::Texture* areiaTextura){
 
+		int t = 0;
+		std::cout << "mapa 1!" << std::endl;
 	        for (int i = 0; i < altura; i++) {
 	            for (int j = 0; j < largura; j++) {
-	                casas[i][j] = sf::RectangleShape(sf::Vector2f(tamanhoCasa, tamanhoCasa));
-	                casas[i][j].setPosition(j * tamanhoCasa, i * tamanhoCasa);
-	                if (mapa[i][j] == '#') {
-	                    casas[i][j].setFillColor(sf::Color::Blue);
-		                casas[i][j].setOutlineThickness(1);
-		                casas[i][j].setOutlineColor(sf::Color::Black);
+	            	std::cout << "mapa 2!" << std::endl;
+	                casas[i][j].corpo = sf::RectangleShape(sf::Vector2f(tamanhoCasa, tamanhoCasa));
+	                casas[i][j].corpo.setPosition(j * tamanhoCasa, i * tamanhoCasa);
+	                t++;
+	                std::cout << t << std::endl;
+	                std::cout << i << std::endl;
+	                std::cout << j  << std::endl;
+	                if (mapa[i][j] == '#'){
+	                	std::cout << "mapa 3!" << std::endl;
+	                    casas[i][j].corpo.setTexture(areiaTextura);
+		                casas[i][j].solido = true;
 	                }else if(mapa[i][j] == '*'){
-	                    casas[i][j].setFillColor(sf::Color::Green);
+	                	std::cout << "mapa 4!" << std::endl;
+	                	casas[i][j].solido = false;
 	                }else if(mapa[i][j] == '1'){
+	                	std::cout << "mapa 5!" << std::endl;
 	                    jogador1.setPosi(j * tamanhoCasa, i * tamanhoCasa);
-	                    casas[i][j].setFillColor(sf::Color::Black);
+	                    casas[i][j].solido = false;
 	                }else if(mapa[i][j] == '2'){
+	                	std::cout << "mapa 6!" << std::endl;
 	                    jogador2.setPosi(j * tamanhoCasa, i * tamanhoCasa);
-	                    casas[i][j].setFillColor(sf::Color::Black);
+	                    casas[i][j].solido = false;
 	                }else{
-	                    casas[i][j].setFillColor(sf::Color::Black);
+	                	std::cout << "mapa 7!" << std::endl;
+	                	casas[i][j].solido = false;
 	                }
-
 	            }
 	        }
 	        std::cout << "Mapa definido!" << std::endl;
 	    }
 
-	sf::RectangleShape (*getCasa())[40]{
+//	sf::RectangleShape (*getCasa())[40]{
+//		return casas;
+//	}
+
+	Bloco  (*getCasa())[40]{
 		return casas;
 	}
 
@@ -81,8 +96,8 @@ public:
 	void desenharMapa(sf::RenderWindow& window){
 		for (int i = 0; i < altura; i++) {
 			for (int j = 0; j < largura; j++) {
-				if(casas[i][j].getFillColor() != sf::Color::Black)
-				window.draw(casas[i][j]);
+				if(casas[i][j].solido)
+				window.draw(casas[i][j].corpo);
 		    }
 		}
 		std::cout << "Mapa desenhado!" << std::endl;
